26ff544ab7b9b8793e00ccb67f6802b50f85d247
Daiv McBride <daivmcbride@yahoo.com>
2014-09-22 09:23:22 -0500

    Daiv commit

--- DruryExplorer/app/src/main/java/edu/drury/mcs/icarus/druryexplorer/DruryMap.java
+++ DruryExplorer/app/src/main/java/edu/drury/mcs/icarus/druryexplorer/DruryMap.java
@@ -60,6 +60,6 @@ public class DruryMap extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.219736, -93.28576900000002)).title("Marker"));
     }
 }
