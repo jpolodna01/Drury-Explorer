7b43efaafc9ce4cd8cc6779e726004af20050762
Daniel Chick <daniel.chick@hotmail.com>
2014-11-21 22:32:21 -0600

    Database has been populated with halls and departments. We now have a database manager for connecting to and querying the database as well as functions in the Hall class for utilizing the database manager.

--- DruryExplorer/app/src/main/java/edu/drury/mcs/icarus/druryexplorer/DataManager.java
+++ DruryExplorer/app/src/main/java/edu/drury/mcs/icarus/druryexplorer/DataManager.java
@@ -2,119 +2,282 @@ package edu.drury.mcs.icarus.druryexplorer;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.SQLException;
+import android.location.Location;
+import android.util.Log;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by Daniel Chick on 10/26/2014.
+ * Created by Daniel Chick && Josef Polodna on 10/26/2014.
+ *
+ * This class is used to assist in the creation and version management of the database. Much of the original functionality
+ * of creating a database was overrode so that we could simply use a pre-made database created outside of android studio.
+ *
  */
 public class DataManager extends SQLiteOpenHelper
 {
+
     private static final int DATABASE_VERSION = 1;
+	
+	// path to db
+    //josef edited file path
+	private static String DB_PATH = "/data/data/edu.drury.mcs.icarus.druryexplorer/databases";
 
-    private static final String DATABASE_NAME = "hallManager",
-        TABLE_HALL = "Hall",
-        KEY_ID ="id",
-        KEY_NAME = "name",
-        KEY_YEAR = "year",
-        KEY_HISTORY = "history";
+    private static final String DATABASE_NAME = "hallManager.sqlite";
 
+    private String path = DB_PATH + DATABASE_NAME;
+
+    private final Context context;
+    private SQLiteDatabase db;
+
+    // pulling from the db, so not necessary
+	private String TABLE_HALL = "Hall",
+       KEY_ID ="id",
+       KEY_NAME = "name",
+       KEY_YEAR = "year",
+       KEY_HISTORY = "history";
+
+    /**
+     * Constructor for class with context variable
+     *
+     * @param context
+     */
     public DataManager(Context context)
     {
+        // Calls parent class
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+		// update: set the context for the db
+		this.context=context;
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db)
-    {
-        db.execSQL("CREATE TABLE" + TABLE_HALL + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_NAME + " TEXT," + KEY_YEAR + " TEXT," + KEY_HISTORY + " TEXT)");
+    /**
+     * Create a database and override it with our created database
+     *
+     * @throws IOException
+     */
+	 public void create() throws IOException{
+
+		boolean dbExist = checkDataBase();
+
+		if(dbExist){
+		  //do nothing - database already exist
+		}
+        else{
+
+		  //By calling this method and empty database will be created into the default system path
+		  //of your application so we are gonna be able to overwrite that database with our database.
+			this.getWritableDatabase();
+
+			try {
+
+			copyDataBase();
+
+		  } catch (IOException e) {
+
+			throw new Error("Error copying database");
+
+		  }
+		}
+	}
+	
+	//update: check database existance to prevent re-copying the data
+
+    /**
+     * Check to see if the database exists to prevent re-copying data into it
+     *
+     * @return = boolean, depends on existence of database
+     */
+	private boolean checkDataBase(){
+
+        File dbFile = new File(DB_PATH + DATABASE_NAME);
+        return dbFile.exists();
+	  }
+
+    /**
+     * Copy the database assets to the new new system database
+     *
+     * @throws IOException
+     */
+	private void copyDataBase() throws IOException {
+        //Open your local db as the input stream
+        InputStream myInput = context.getAssets().open(DATABASE_NAME);
+
+        // Path to the just created empty db
+        String outFileName = DB_PATH + DATABASE_NAME;
+
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-    {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_HALL);
+	//update: Open the database
 
-        onCreate(db);
-    }
+    /**
+     * Open the database
+     *
+     * @return = boolean depending on if opened
+     */
+	public boolean open() {
+		try {
+		  //String myPath = DB_PATH + DATABASE_NAME; //using global var for now
+		  db = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READWRITE);
+		  return true;
+		} 
+		catch(SQLException sqle) {
+		  db = null;
+		  return false;
+		}
+	}
 
-    public void createHall(Halls hall)
-    {
-        SQLiteDatabase db = getWritableDatabase();
+    /**
 
-        ContentValues values = new ContentValues();
+     * Close the database
+     *
+     */
+	@Override
+	public synchronized void close(){
+		if(db != null) {
+			db.close();
+		}
+		
+		super.close();
+	}
+	
+	/*
+		Given that we are replacing the functionality of the standard database set-
+		up, we have to override the base functions provided by the SQLiteHelper since we are
+		merely drawing data from an already created database.
+	*/
 
-        values.put(KEY_NAME, hall.getName());
-        values.put(KEY_YEAR, hall.getYear());
-        values.put(KEY_HISTORY, hall.getHistory());
+    /**
+     * Appropriately override the onCreate function to do nothing on create
+     *
+     * @param db
+     */
+	@Override
+	public void onCreate(SQLiteDatabase db){}
 
-        db.insert(TABLE_HALL, null, values);
-        db.close();
-    }
+    /**
+     * This method details what to do when upgrading the database, which is nothing as it was above
+     *
+     * @param db
+     * @param oldVersion
+     * @param newVersion
+     */
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){}
+	
+	
+	/*
+		Public methods to access the database
+	*/
 
-    public Halls getHall(int id)
-    {
-        SQLiteDatabase db = getReadableDatabase();
+    /**
+     * Gets the halls and all associated data
+     *
+     * @return
+     */
+	public List<Halls> getHalls()
+	{
+		List<Halls> hallz = new ArrayList<Halls>();
+		
+		try {
 
-        Cursor cursor = db.query(TABLE_HALL, new String[] { KEY_ID, KEY_NAME, KEY_YEAR, KEY_HISTORY }, KEY_ID + "=?", new String[] {String.valueOf(id)}, null, null, null);
+            String query = "SELECT * FROM" + TABLE_HALL;
 
-        if(cursor !=null)
-            cursor.moveToFirst();
+            db = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READWRITE);
+            open();
+            Cursor cursor = db.rawQuery(query, null);
 
-        Halls hall = new Halls(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3));
+            if (cursor.moveToFirst())
+            {
+                do
+                {
+
+                    Halls hallObject = new Halls();
+                    hallObject.setID(Integer.parseInt(cursor.getString(0)));
+                    hallObject.setName(cursor.getString(1));
+                    hallObject.setYear(Integer.parseInt(cursor.getString(2)));
+                    hallObject.setHistory(cursor.getString(3));
+
+                    hallz.add(hallObject);
+
+                }
+                while (cursor.moveToNext());
+            }
+        }
+		catch(Exception e) 
+		{
+		  // sql error
+		}
+
+		return hallz;
+	}
+
+    /**
+     * Collects the hall names to be projected onto the screen
+     *
+     * @return
+     */
+    public List<Halls> getHallList(){
+        List<Halls> hall = null;
+
+        try{
+
+            String query = "SELECT Hall.Name FROM " + TABLE_HALL;
+
+            SQLiteDatabase db = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READWRITE);
+
+            Cursor cursor = db.rawQuery(query, null);
+
+            hall = new LinkedList<Halls>();
+            if(cursor.moveToFirst())
+            {
+                do {
+
+                    Halls hallObject = new Halls();
+                    //hallObject.setID(Integer.parseInt(cursor.getString(0)));
+                    hallObject.setName(cursor.getString(1));
+                    //hallObject.setYear(Integer.parseInt(cursor.getString(2)));
+                    // hallObject.setHistory(cursor.getString(3));
+
+                    hall.add(hallObject);
+                }
+                while (cursor.moveToNext());
+            }
+
+
+        }
+        catch(Exception e){
+            //sql error
+        }
+
         return hall;
     }
 
-    public void deleteHall(Halls hall)
-    {
-        SQLiteDatabase db = getWritableDatabase();
-        db.delete(TABLE_HALL, KEY_ID + "=?", new String[] {String.valueOf(hall.getID())});
-        db.close();
-    }
-
-    public int getHallCount()
-    {
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_HALL, null);
-        cursor.close();
-
-        return cursor.getCount();
-    }
-
-    public int updateHall(Halls hall)
-    {
-        SQLiteDatabase db = getReadableDatabase();
-
-        ContentValues values = new ContentValues();
-
-        values.put(KEY_NAME, hall.getName());
-        values.put(KEY_YEAR, hall.getYear());
-        values.put(KEY_HISTORY, hall.getHistory());
-
-        return db.update(TABLE_HALL, values, KEY_ID + "=?", new String[] { String.valueOf(hall.getID()) });
-    }
-
-    public List<Halls> getAllHalls()
-    {
-        List<Halls> halls = new ArrayList<Halls>();
-
-        SQLiteDatabase db = getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM" + TABLE_HALL, null);
-
-        if(cursor.moveToFirst())
-        {
-            do{
-                Halls hall = new Halls(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3));
-
-                halls.add(hall);
-            }
-
-            while(cursor.moveToNext());
-        }
-        return halls;
-    }
 }
--- DruryExplorer/app/src/main/java/edu/drury/mcs/icarus/druryexplorer/Halls.java
+++ DruryExplorer/app/src/main/java/edu/drury/mcs/icarus/druryexplorer/Halls.java
@@ -1,33 +1,86 @@
 package edu.drury.mcs.icarus.druryexplorer;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 
 public class Halls extends Activity {
 
-    private String _name, _year, _history;
-    private int id;
+    public String _name, _history;
+    public int _id, _year;
 
-    public Halls(int id, String name, String year, String history) {
-        _name = name;
-        _year = year;
-        _history = history;
+    //empty constructor
+    public Halls(){
+
     }
 
+
+    public Halls(int id, String name, int year, String history)
+    {
+        this._id=id;
+        this._name = name;
+        this._year = year;
+        this._history = history;
+    }
+
+    public Halls(String name, int year, String history)
+    {
+        this._name = name;
+        this._year = year;
+        this._history = history;
+    }
+
+
+    //Get Methods
     public int getID(){
-        return id;
+        return this._id;
     }
-    public String getName(){return _name;}
-    public String getYear(){return _year;}
-    public String getHistory(){return _history;}
+    public String getName(){return this._name;}
+    public int getYear(){return this._year;}
+    public String getHistory(){return this._history;}
 
+    //set Methods
+    public void setID(int ID){this._id = ID;}
+    public void setName(String name){this._name=name;}
+    public void setYear(int year){this._year=year;}
+    public void setHistory(String history){this._history=history;}
+
+    DataManager db=new DataManager(Halls.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halls);
+
+        preLoadDb();
+        populateListView();
+    }
+
+    public void populateListView()
+    {
+        DataManager entry = new DataManager(Halls.this);
+
+        List<Halls> all = entry.getHallList();
+        if(all.size()>0) // check if list contains items.
+        {
+            ListView lv = (ListView) findViewById(R.id.hallView);
+            ArrayAdapter<Halls> arrayAdapter = new ArrayAdapter<Halls>(this, android.R.layout.simple_list_item_1, all);
+
+            lv.setAdapter(arrayAdapter);
+        }
+        else
+        {
+            Toast.makeText(Halls.this, "Daniel says no data from DB", Toast.LENGTH_LONG).show();
+        }
     }
 
 
@@ -48,6 +101,20 @@ public class Halls extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void preLoadDb()
+    {
+        DataManager db = new DataManager(this);
+
+        try
+        {
+            db.create();
+        }
+        catch(IOException ioe)
+        {
+            throw new Error("Unable to create Database");
+        }
     }
 
 }
