511ab3d3e87768e3a5d0ab9b1609609dd4b73091
Daniel Chick <daniel.chick@hotmail.com>
2015-02-25 22:39:31 -0600

    grabs the name of the hall from the bundle passed from the hall list

--- DruryExplorer/app/src/main/java/edu/drury/mcs/icarus/druryexplorer/HallFacts.java
+++ DruryExplorer/app/src/main/java/edu/drury/mcs/icarus/druryexplorer/HallFacts.java
@@ -1,25 +1,57 @@
 package edu.drury.mcs.icarus.druryexplorer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class HallFacts extends ActionBarActivity {
+public class HallFacts extends Activity {
+
+    public TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hall_facts);
+        textView1 = (TextView) findViewById(R.id.hNameView);
+
+        //creates a string to place the clicked object
+        String newName;
+        if(savedInstanceState == null)
+        {
+            //get the bundle from Halls.java
+            Bundle extras = getIntent().getExtras();
+
+            //if there is nothing there then the string is empty
+            if(extras == null)
+            {
+                newName = null;
+            }
+
+            //if there is something there the get the string from the bundle
+            else
+            {
+                newName = extras.getString("clickedHall");
+            }
+        }
+        else
+        {
+            newName = (String) savedInstanceState.getSerializable("clickedHall");
+        }
+
+        //display the name of the clicked hall
+        textView1.setText(newName);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hall_facts, menu);
+        getMenuInflater().inflate(R.menu.navigation, menu);
         return true;
     }
 
